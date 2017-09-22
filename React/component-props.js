448628cd26177ea2090406aa48dfb42025b99280
Vlad Shostak <VeeShostak@users.noreply.github.com>
2017-09-21 22:07:29 -0400

    Create component-props.js

---
+++ React/component-props.js
@@ -0,0 +1,85 @@
+
+
+// custom html elements = react components
+//
+// component props (key value): setting components data on initialization
+// Creates one way communication
+//
+// this: reference to this instance of this component
+
+
+class IndecisionApp extends React.Component {
+  render() {
+    const title = 'Indecision';
+    const subtitle = 'Put your life in the hands of a computer';
+    const options = ['one', 'two', 'three'];
+
+    return (
+      <div>
+        <Header title={title} subtitle={subtitle} />
+        <Action />
+        <Options options={options}/>
+        <AddOption />
+      </div>
+    );
+  }
+}
+
+
+class Header extends React.Component {
+  render() {
+    this.props
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        <h2>{this.props.subtitle}</h2>
+      </div>
+    );
+  }
+}
+class Action extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>What to do?</button>
+      </div>
+
+    );
+  }
+}
+
+class Options extends React.Component {
+  render() {
+    return (
+      <div>
+        {
+          this.props.options.map((option) => <Option key={option} optionText={option} />)
+        }
+
+      </div>
+    );
+  }
+}
+
+class Option extends React.Component {
+  render() {
+    return (
+      <div>
+        Option: {this.props.optionText}
+      </div>
+    );
+  }
+}
+
+class AddOption extends React.Component {
+  render() {
+    return (
+      <div>
+        AddOption Component
+      </div>
+    );
+  }
+}
+
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
