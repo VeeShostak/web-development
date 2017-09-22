51253efa1bf9fd80ca968f11186927895be0324b
Vlad Shostak <VeeShostak@users.noreply.github.com>
2017-09-22 00:03:44 -0400

    Create events-and-method-binding.js

---
+++ React/events-and-method-binding.js
@@ -0,0 +1,105 @@
+
+
+
+class IndecisionApp extends React.Component {
+  render() {
+    const title = 'Indecision';
+    const subtitle = 'Put your life in the hands of a computer';
+    const options = ['one', 'two', 'three'];
+
+    return (
+      <div>
+        <Header title={title} subtitle={subtitle} />
+        <Action />
+        <Options options={options}/>
+        <AddOption />
+      </div>
+    );
+  }
+}
+
+
+class Header extends React.Component {
+  render() {
+    this.props
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        <h2>{this.props.subtitle}</h2>
+      </div>
+    );
+  }
+}
+class Action extends React.Component {
+  handlePick() {
+    alert('handlePick');
+
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handlePick}>What to do?</button>
+      </div>
+
+    );
+  }
+}
+
+class Options extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleRemoveAll = this.handleRemoveAll.bind(this);
+  }
+  handleRemoveAll(){
+    alert(this.props.options);
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleRemoveAll}>Remove All</button>
+        {
+          this.props.options.map((option) => <Option key={option} optionText={option} />)
+        }
+
+      </div>
+    );
+  }
+}
+
+class Option extends React.Component {
+  render() {
+    return (
+      <div>
+        Option: {this.props.optionText}
+      </div>
+    );
+  }
+}
+
+class AddOption extends React.Component {
+  handleAddOption (e) {
+    e.preventDefault(); // stop full page refresh
+
+    const option = e.target.elements.option.value.trim();
+
+    if (option) {
+      alert(option);
+      e.target.elements.option.value = '';
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleAddOption}>
+          <input type="text" name="option"></input>
+          <button>Add Option</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
