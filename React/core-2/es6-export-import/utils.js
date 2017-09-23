0e053cd7c9778e3aca24de52b796dcd2232e4ebb
Vlad Shostak <VeeShostak@users.noreply.github.com>
2017-09-22 21:06:14 -0400

    Create utils.js

---
+++ React/core-2/es6-export-import/utils.js
@@ -0,0 +1,10 @@
+console.log('utils.js running');
+
+const square = (x) => x * x;
+
+const add = (a, b) => a + b;
+
+export { square, add };
+
+// or export variables inline of declaration of variabnle
+// export const add = (a, b) => a + b;
