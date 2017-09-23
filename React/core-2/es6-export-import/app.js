e064f3c5f3638ae5a26ca6d9ef38b46b491a28f1
Vlad Shostak <VeeShostak@users.noreply.github.com>
2017-09-22 21:05:16 -0400

    Create app.js

---
+++ React/core-2/es6-export-import/app.js
@@ -0,0 +1,11 @@
+
+// ENTRY
+// import { square, add } from './utils.js'
+//
+// console.log("app.js running!");
+// console.log(square(5));
+// console.log(add(25,3));
+
+import { isAdult, canDrink } from './person.js'
+
+console.log(isAdult(18), canDrink(15));
