f99db66485ea18ed26475c7c7f5e299e07ec03f0
Vlad Shostak <VeeShostak@users.noreply.github.com>
2017-10-19 00:44:08 -0400

    Create es6-promises-asynchronous.js

---
+++ javascript/es6-promises-asynchronous.js
@@ -0,0 +1,33 @@
+
+
+// asynchronous js
+
+// resolve we got the data. reject, something went wrong
+// resolve or reject only a single time
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // resolve({
+    //   name: 'John',
+    //   age: 23
+    // });
+    reject('Error message');
+  }, 6000);
+});
+
+console.log('before');
+
+// .then() register a callback when and if a promise resolves
+promise.then((data) => {
+  console.log('1', data);
+}).catch((error) => {
+  console.log('error: ', error);
+});
+
+
+console.log('after');
+
+
+// before, after, then 6s later error prints
+
+// most of the time promises are made by the library (like firebase),
+// we just have to handle them
